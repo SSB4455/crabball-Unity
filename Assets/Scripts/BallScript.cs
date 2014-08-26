df32e160e40dd8f860ba192c798921998b01a556
SSB4455 <741086@gmail.com>
2014-08-26 10:28:09 +0800

    背景 弹力球

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void FixedUpdate ()
+	{
+		Debug.Log ("FixedUpdate v="+rigidbody2D.velocity);
+	}
+	
+	void OnCollisionEnter2D(Collision2D collisionInfo)
+	{
+		Vector2 oldV = rigidbody2D.velocity;
+		Debug.Log ("OnCollisionEnter2D oldV="+oldV);
+		//rigidbody2D.velocity = new Vector2 (oldV.x, -3 * oldV.y);
+		
+	}
+	
+}
---
+++ Assets/Scripts/FloorScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorScript : MonoBehaviour {
+
+	public bool isFloor = true;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+		Debug.Log ("floor");
+	}
+	
+	void OnTriggerEnter (Collider other)
+	{
+		
+		Debug.Log ("floor OnTriggerEnter");
+	}
+}
